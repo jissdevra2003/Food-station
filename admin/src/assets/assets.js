31b87ae893b488c07f5c6afd9900dcf6bbd54cb8
Jiss devra <96423636+jissdevra2003@users.noreply.github.com>
2025-02-19 18:08:14 +0530

    updated gitignore in frontend admin

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,22 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import list_icon from './list.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+import remove_icon from './remove-30.png';
+
+export const assets ={
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon,
+    list_icon,
+    remove_icon
+
+}
+
+export const url = 'http://localhost:4000'
